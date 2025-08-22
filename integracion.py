a8e2fbdc7d576314cae0365a79f7dd1fe50ffc96
htabilo <htabilo@gmail.com>
2025-08-22 17:51:37 -0400

    Fourth commit

--- integracion.py
+++ integracion.py
@@ -1,27 +1,50 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-
-def clic_en_boton():
-    # Ya no necesitas WebDriverManager en el CI,
-    # el controlador se gestiona en el archivo .yml
-    # service = Service(ChromeDriverManager().install())
-    
-    # El controlador de Chrome ya está en el PATH del sistema
-    # gracias a la configuración de GitHub Actions.
-    driver = webdriver.Chrome()
-
-    # Abre una página con al menos un botón para hacer clic
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
-
-    boton = driver.find_element(By.CSS_SELECTOR, "button")
-    boton.click()
-    
-    time.sleep(3)
-    driver.quit()
+def initialize_driver():  
+    options = Options()
+    options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=options)
+    return driver     
 
 
-if __name__ == "__main__":
-    clic_en_boton()
+def login(driver):
+    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, "username"))
+        )
+    driver.find_element(By.NAME, "username").send_keys("Admin")
+    driver.find_element(By.NAME, "password").send_keys("admin123")
+    driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button').click()
+    WebDriverWait(driver, 10).until(EC.url_contains("dashboard"))
+    print("login ok")
+
+
+def ir_a_PIM(driver):
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//span[text()="PIM"]'))
+    ).click()
+    print("entrando a PIM")
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//h5[text()="Employee Information"]'))
+    )
+
+             
+
+
+
+def main():  
+    driver = initialize_driver()
+    try:
+        login(driver)
+        ir_a_PIM(driver)
+        time.sleep(3)
+    finally:
+        driver.quit()
+if __name__ == '__main__':  
+    main()
 
